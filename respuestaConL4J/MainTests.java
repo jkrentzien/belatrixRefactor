b6bd5767c9962a42c3643a1d91040b64fafe1fe9
jkrentzien <jkdsz9@gmail.com>
2019-07-23 10:53:16 -0500

    Update MainTests.java

--- respuestaConL4J/MainTests.java
+++ respuestaConL4J/MainTests.java
@@ -10,7 +10,7 @@ public class MainTests {
 	public static void main(String[] args) {
 		
 		//CONFIG INVALIDO -EXCEPTION
-		JobLogger logger1 = new JobLogger(false,false,false,false,false,false);
+		JobLogger logger1 = new JobLogger2(false,false,false,false,false,false);
 		try {
 			JobLogger.LogMessage("HOLA1", JobLevel.ERROR);
 		} catch (Exception e) {
@@ -19,7 +19,7 @@ public class MainTests {
 		}
 		
 		//ESPECIFICAR TIPO DE ERROR A LOGUEAR -EXCEPTION
-		JobLogger logger2 = new JobLogger(true,false,false,false,false,false);
+		JobLogger logger2 = new JobLogger2(true,false,false,false,false,false);
 		try {
 			JobLogger.LogMessage("HOLA2", JobLevel.ERROR);
 		} catch (Exception e) {
@@ -28,7 +28,7 @@ public class MainTests {
 		}
 		
 		//MENSAJE NULO- NO SE OBSERVA EN CONSOLA
-		JobLogger logger3 = new JobLogger(true,false,false,true,false,false);
+		JobLogger logger3 = new JobLogger2(true,false,false,true,false,false);
 		try {
 			JobLogger.LogMessage("HOLA3", JobLevel.ERROR);
 			System.out.println("NULL");
@@ -38,7 +38,7 @@ public class MainTests {
 		}
 		
 		//MENSAJE SE OBSERVA EN CONSOLA
-		JobLogger logger4 = new JobLogger(false,true,false,true,false,true);
+		JobLogger logger4 = new JobLogger2(false,true,false,true,false,true);
 		try {
 			JobLogger.LogMessage("HOLA4", JobLevel.ERROR);
 			JobLogger.LogMessage("HOLA5", JobLevel.MESSAGE);
@@ -48,7 +48,7 @@ public class MainTests {
 		}
 		
 		//MENSAJE EN BD -> en Consola
-		JobLogger logger5 = new JobLogger(false,false,true,true,false,true);
+		JobLogger logger5 = new JobLogger2(false,false,true,true,false,true);
 		try {
 			JobLogger.LogMessage("HOLA6", JobLevel.ERROR);
 		} catch (Exception e) {
@@ -57,7 +57,7 @@ public class MainTests {
 		}
 		
 		//MENSAJE EN FILE
-		JobLogger logger6 = new JobLogger(true,false,false,false,true,false);
+		JobLogger logger6 = new JobLogger2(true,false,false,false,true,false);
 		try {
 			JobLogger.LogMessage("HOLA7", JobLevel.WARNING);
 		} catch (Exception e) {
@@ -66,7 +66,7 @@ public class MainTests {
 		}
 		
 		//MENSAJE EN FILE y CONSOLA
-		JobLogger logger7 = new JobLogger(true,false,true,false,true,false);
+		JobLogger logger7 = new JobLogger2(true,false,true,false,true,false);
 		try {
 			JobLogger.LogMessage("HOLA8", JobLevel.WARNING);
 		} catch (Exception e) {
